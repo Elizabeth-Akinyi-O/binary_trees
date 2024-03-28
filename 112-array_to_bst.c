86a7cc226d9f84d83212def7bd62065b524081c4
Elizabeth-Akinyi-O <lizbethakinyio@gmail.com>
2024-03-28 05:29:05 +0300

    BST - Array to BST

---
+++ 112-array_to_bst.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+
+/**
+ * array_to_bst - builds a Binary Search Tree from an array
+ * @array: pointer to the first element of the array to be converted
+ * @size: number of element in the array
+ *
+ * Return: pointer to the root node of the created BST, or NULL on failure
+ */
+bst_t *array_to_bst(int *array, size_t size)
+{
+	bst_t *tree = NULL;
+	size_t a, b;
+
+	if (array == NULL)
+		return (NULL);
+
+	for (a = 0; a < size; a++)
+	{
+		for (b = 0; b < a; b++)
+		{
+			if (array[b] == array[a])
+				break;
+		}
+		if (b == a)
+		{
+			if (bst_insert(&tree, array[a]) == NULL)
+				return (NULL);
+		}
+	}
+
+	return (tree);
+}
